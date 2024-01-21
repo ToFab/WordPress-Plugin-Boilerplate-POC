f4bb05265cddc10aab27e443311398625b787516
tony <tony@shovl.ai>
2024-01-21 11:09:14 +0700

    Initial checkin

---
+++ plugin-name/ts-shared/model/class-one.ts
@@ -0,0 +1,12 @@
+export class classOne {
+    
+    private readonly name: string;
+    
+    constructor(name: string) {
+        this.name = name;
+    }
+
+    getName() {
+        return this.name;
+    }    
+}
---
+++ plugin-name/ts-shared/model/class-two.ts
@@ -0,0 +1,12 @@
+export class classTwo {
+    
+    private readonly name: string;
+    
+    constructor(name: string) {
+        this.name = name;
+    }
+
+    getName() {
+        return this.name;
+    }    
+}
